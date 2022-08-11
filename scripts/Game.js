09d523f4744623048face5fbb3692a2edf3673c5
Dave Berzack <daveberzack@gmail.com>
2022-08-10 21:54:35 -0400

    redesign, simplify

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,85 +1,83 @@
-import Canvas from './Canvas.js';
-import DirectCursor from './DirectCursor.js';
-import IterativePainter from './IterativePainter.js';
-import { isInCanvas } from './utils.js';
+import Canvas from "./Canvas.js";
+import DirectCursor from "./DirectCursor.js";
+import PixelPainter from "./PixelPainter.js";
+import { isInCanvas } from "./utils.js";
 
 const GAME_LOOP_INCREMENT = 10;
 
 class Game {
+  constructor() {
+    this.$timerLabel = $("#timer-label");
+    this.hasWon = false;
+    this.isPlaying = false;
+    this.addMouseListeners();
+  }
 
-    constructor(){
-        this.$timerLabel = $("#timer-label");
-        this.$clue = $("#clue");
-        this.hasWon = false;
-        this.isPlaying = false;
-        this.addMouseListeners();
-    }
-    
-    addMouseListeners(){
-        $('body').mousemove(e => {
-            if (this.isPlaying) this.cursor.handleMove(e.clientX,e.clientY);
-        });
+  addMouseListeners() {
+    $("body").mousemove((e) => {
+      if (this.isPlaying) this.cursor.handleMove(e.clientX, e.clientY);
+    });
 
-        $('body').mousedown(e => {
-            if (this.isPlaying) this.handleCursorClick();
-        });
-    }
+    $("body").mousedown((e) => {
+      if (this.isPlaying) this.handleCursorClick();
+    });
+  }
 
-    startGame(challenge) {
-        this.painter?.stopReveal();
-        this.canvas = new Canvas(challenge);
-        this.cursor = new DirectCursor($("#pic"));
-        this.painter = new IterativePainter(this.canvas, this.cursor);
+  startGame(challenge) {
+    this.canvas = new Canvas(challenge);
+    this.cursor = new DirectCursor($("#pic"));
+    this.painter = new PixelPainter(this.canvas, this.cursor);
 
-        this.isPlaying=true;
-        this.timePassed = 0;
-        this.timerWidth = 0;
-        this.$clue.html(challenge.clue);
-        
-        let self = this;
-        clearInterval(this.loopInterval);
-        this.loopInterval = setInterval( () => { self.doGameLoop(); }, GAME_LOOP_INCREMENT );
-        $("#game-buttons").hide();
-    }
-    
-    doGameLoop(){
-        if (!this.isPlaying) return;
-        this.painter.paint();
-        this.updateTimer();
-    }
-    
-    updateTimer(){
-        this.timePassed = this.timePassed + GAME_LOOP_INCREMENT/1000;
-        const w = Math.min(this.timePassed*100/60, 100);
-        this.timerWidth = (this.timerWidth*3+w)/4; //animate
-        this.$timerLabel.width(this.timerWidth+"%");
-        this.$timerLabel.text( Math.floor(this.timePassed) );
-    }
-    
-    handlePenalty(){
-        this.timePassed +=10;
-        this.$timerLabel.css("background-color", "#FF0000");
-        setTimeout( ()=>{
-            this.$timerLabel.css("backgroundColor", "#FFFFFF");
-        },500);
-    }
+    this.isPlaying = true;
+    this.timePassed = 0;
+    this.timerWidth = 0;
 
-    handleWin(){
-        this.hasWon = true;
-        this.isPlaying = false;
-        clearInterval(this.loopInterval);
-        this.painter.revealAll(this.cursor.x, this.cursor.y);
-        $("#game-buttons").show();
-    }
+    let self = this;
+    clearInterval(this.loopInterval);
+    this.loopInterval = setInterval(() => {
+      self.doGameLoop();
+    }, GAME_LOOP_INCREMENT);
+  }
 
-    handleCursorClick(){
-        if ( isInCanvas(this.cursor.x, this.cursor.y, this.canvas.pic.$element) ){
-            const isGuessCorrect = this.canvas.checkGuess(this.cursor.x, this.cursor.y);
-            if (isGuessCorrect) this.handleWin();
-            else this.handlePenalty();
-        }
-        
+  doGameLoop() {
+    if (!this.isPlaying) return;
+    this.painter.paint();
+    this.updateTimer();
+  }
+
+  updateTimer() {
+    this.timePassed = this.timePassed + GAME_LOOP_INCREMENT / 1000;
+    const w = Math.min((this.timePassed * 100) / 60, 100);
+    this.timerWidth = (this.timerWidth * 3 + w) / 4; //animate
+    this.$timerLabel.width(this.timerWidth + "%");
+    this.$timerLabel.text(Math.floor(this.timePassed));
+  }
+
+  handlePenalty() {
+    this.timePassed += 10;
+    this.$timerLabel.css("background-color", "#FF0000");
+    setTimeout(() => {
+      this.$timerLabel.css("backgroundColor", "#FFFFFF");
+    }, 500);
+  }
+
+  handleWin() {
+    this.hasWon = true;
+    this.isPlaying = false;
+    clearInterval(this.loopInterval);
+    this.painter.revealAll(this.cursor.x, this.cursor.y);
+  }
+
+  handleCursorClick() {
+    if (isInCanvas(this.cursor.x, this.cursor.y, this.canvas.pic.$element)) {
+      const isGuessCorrect = this.canvas.checkGuess(
+        this.cursor.x,
+        this.cursor.y
+      );
+      if (isGuessCorrect) this.handleWin();
+      else this.handlePenalty();
     }
+  }
 }
-  
+
 export default Game;
--- scripts/PixelPainter.js
+++ scripts/PixelPainter.js
@@ -1,11 +1,13 @@
 import {rgbToHex, getNewCoordinates, getCanvasCoordinates} from './utils.js';
 import Painter from './Painter.js';
 
-class PixelPainter extends Painter{
+class IterativePainter extends Painter{
 
     constructor(canvas, cursor){
         super(canvas, cursor);
         this.layerResolutions = [256,128,64,32,16,8,4];
+        this.detailStack =[];
+        this.detailFactor=25;
     }
 
     paint() {
@@ -17,108 +19,99 @@ class PixelPainter extends Painter{
         const x = (this.cursor.x-rect.left) * graphicWidth/elementWidth;
         const y = (this.cursor.y-rect.top) * graphicWidth/elementWidth;
 
-        if (x>=0 && y>=0 && x<=this.canvas.graphicWidth && y<=this.canvas.graphicWidth || this.prevX!==undefined) {
+        if (x>=0 && y>=0 && x<=this.canvas.graphicWidth && y<=this.canvas.graphicWidth) {
             
-            const v = Math.sqrt( (x-this.prevX)*(x-this.prevX) +(y-this.prevY)*(y-this.prevY) );
-            if (v<.1) return;
-            else if (v<2){
-                for (let i=0; i<5; i++){
-                    const px = x + (Math.random()-.5)*20;
-                    const py = y + (Math.random()-.5)*20;
-                    const r = Math.random()>.5 ? 1:2;
-                    this._stamp(px,py,r,7);
-                }
-                this._sprayPixels(x,y,this.layerResolutions[6],6, 1);
-                this._sprayPixels(x,y,this.layerResolutions[5],5, 1);
-            }
-            else {
-                const layerIndex= this._getLayerIndex(v);
-                this._sprayPixels(x,y,this.layerResolutions[layerIndex],layerIndex, 2);
-                if(layerIndex>0) this._sprayPixels(x,y,this.layerResolutions[layerIndex-1],layerIndex-1, 1);
-            }
-
+            const xOffset = (Math.random()-.5)*2 *15;
+            const yOffset = (Math.random()-.5)*2 *15;
+            this._doPaint(x+xOffset, y+yOffset);
+            this._doPaint(x, y);
+            
         }
-        this.prevX=x;
-        this.prevY=y;
+    }
+
+    _doPaint(x,y){
+        //get the detail level from the detail canvas
+        const detailLevel = this._getDetailLevel(x, y) +1;
+
+        const c = detailLevel*this.detailFactor;
+        const hex = "#"+("000000"+rgbToHex(c, c, c)).slice(-6);
+        const w = this.layerResolutions[detailLevel];
+        const ctx = this.canvas.detail.context;
+        this.detailStack.push({x,y,w,hex,ctx});
+
+        if (this.detailStack.length>3){
+            const d = this.detailStack.shift();
+            this._pixel(d.x,d.y,d.w,d.hex,d.ctx);
+        }
+        
+        if (detailLevel<7){
+            const p = this.canvas.pic.context.getImageData(x, y, 1, 1).data;
+            const hex2 = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+            const ctx2 = this.canvas.layers[detailLevel].context;
+            this._pixel(x,y,this.layerResolutions[detailLevel], hex2, ctx2);
+        }
+        else {
+            this._stamp(x,y,8,this.canvas.layers[detailLevel].context)
+        }
     }
 
     revealAll(clickX, clickY){
-        const $canvas = this.canvas.pic.$element;
-        const {x, y} = getCanvasCoordinates(clickX, clickY, $canvas);
-        const dimension = $canvas.attr("width");
-        const minV = 1;
-        const maxV = 20;
-        const appearFrames = 300;
-        let appearCounter=0;
-        const margin=50;
-
-        if (this.appearInterval) clearInterval(this.appearInterval);
-        this.appearInterval = setInterval(()=>{
-            appearCounter++;
-            appearCounter++;
-
-            let opacity = Math.pow(appearCounter/appearFrames, 2);
-            pic.style.opacity = opacity;
-
-            const vFactor = minV + (maxV-minV)* (1-appearCounter/appearFrames);
-            for (let i=0; i<200; i++){
-                const v = Math.random()*vFactor*3*appearCounter/appearFrames;
-                const d = (Math.random()+.25) * dimension * appearCounter/appearFrames +v*10;
-                const a = Math.random()*Math.PI*2;
-                const {x:x1, y:y1} = getNewCoordinates(x, y, a, d);
-                const layerIndex = this._getLayerIndex(v);
-                const res = this.layerResolutions[layerIndex];
-                if (x1>-margin && y1>-margin && x1<dimension+margin && y1<dimension+margin){
-                    this._pixel(x1, y1, res, layerIndex);
+            const $canvas = this.canvas.pic.$element;
+            const {x, y} = getCanvasCoordinates(clickX, clickY, $canvas);
+            const dimension = $canvas.attr("width");
+            const minV = 1;
+            const maxV = 20;
+            const appearFrames = 1200;
+            let appearCounter=0;
+            const margin=50;
+    
+            if (this.appearInterval) clearInterval(this.appearInterval);
+            this.appearInterval = setInterval(()=>{
+                appearCounter++;
+                appearCounter++;
+    
+                let opacity = Math.pow(appearCounter/appearFrames, 2); //slowly increase exponentially
+                pic.style.opacity = Math.max(0, 4*opacity-.75); //only appear during the last 1/4 of the animation
+    
+                const vFactor = minV + (maxV-minV)* (1-appearCounter/appearFrames);
+                for (let i=0; i<100; i++){
+                    const v = Math.random()*vFactor*3*appearCounter/appearFrames;
+                    const d = (Math.random()+.25) * dimension * appearCounter/appearFrames +v*10;
+                    const a = Math.random()*Math.PI*2;
+                    const {x:x1, y:y1} = getNewCoordinates(x, y, a, d);
+                    if (x1>-margin && y1>-margin && x1<dimension+margin && y1<dimension+margin){
+                        this._doPaint(x1, y1);
+                    }
+                    
                 }
-                
-            }
-
-            if (appearCounter>appearFrames) clearInterval(this.appearInterval);
-        },10);
-        
-    }
+    
+                if (appearCounter>appearFrames) clearInterval(this.appearInterval);
+            },10);
+        }
 
     stopReveal(){
         if (this.appearInterval) clearInterval(this.appearInterval);
     }
 
-    _stamp(x,y,r,layer){
-        const d = this.canvas.pic.context.getImageData(x-r, y-r, 2*r+1, 2*r+1);
-        if (d) this.canvas.layers[layer].context.putImageData(d, x-r, y-r);
+    _getDetailLevel(x,y){
+        const p = this.canvas.detail.context.getImageData(x, y, 1, 1).data;
+        return Math.floor(p[0]/this.detailFactor);
     }
 
-    _pixel(x,y,res,layerIndex){
-        const p = this.canvas.pic.context.getImageData(x, y, 1, 1).data;
-        const hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-        const paintX = Math.floor(x/res)*res;
-        const paintY = Math.floor(y/res)*res;
+    _stamp(x,y,w,ctx){
+        const d = this.canvas.pic.context.getImageData(x-w/2, y-w/2, w, w);
+        if (d) ctx.putImageData(d, x-w/2, y-w/2);
+    }
 
-        const ctx = this.canvas.layers[layerIndex].context;
-        ctx.fillStyle = hex;
+    _pixel(x,y,w,color,ctx){
+        const paintX = Math.floor(x/w)*w;
+        const paintY = Math.floor(y/w)*w;
+        ctx.fillStyle = color;
         ctx.beginPath();
-        ctx.rect(paintX, paintY, res, res);
+        ctx.rect(paintX, paintY, w, w);
         ctx.fill();
     }
 
-    _sprayPixels(x,y,res,layerIndex, n){
-        this._pixel(x, y, res, layerIndex);
-        for (let i=0; i<n-1; i++){
-            let px = Math.max(0, x + (Math.random()-.5) *res*3 );
-            let py = Math.max(0, y + (Math.random()-.5) *res*3 );
-            this._pixel(px, py, res, layerIndex);
-        }
-    }
-
-    _getLayerIndex(v){
-        if      (v<2)  return 6;
-        else if (v<4)  return 5;
-        else if (v<7) return 4;
-        else if (v<12) return 3;
-        else if (v<20) return 2;
-        else if (v<35) return 1;
-        else return 0;
-    }
 }
   
-export default PixelPainter;
+export default IterativePainter;
